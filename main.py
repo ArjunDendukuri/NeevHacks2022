7c84bb6bcb7600e37efbe036641a6eb90dd9d67d
ArjunDendukuri <arjundendukuri@kchigh.com>
2022-03-12 19:29:04 +0530

    Fix Main and Run TK

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import json
 from item.Item import Item
 from item.Item import json_paths
 from tkinter import *
+import os
+from GUI.FullGUI import tk_run
 
 items = []
 
 
 def parse(file_name):
+    scr_name = os.path.dirname(__file__)
+    name = scr_name + "\\" + file_name
     try:
-        file = open(file_name,'r')
+        with open(name, 'r') as f:
+            data = json.load(f)
+            raw_data = f.read()
+            json_data = json.load(raw_data)
+            ret = Item(json_data['name'], json_data['description'], json_data['rarity']) 
+            f.close()
+            print("Loaded data from",file_name) #temp
+            return ret
     except:
-        print("File {} is not found".format(file_name))
         return None
-    raw_data = file.read()
-    json_data = json.dumps(raw_data)
-    ret = Item(json_data['name'], json_data['description'], json_data['rarity']) 
-    file.close()
-    print("Loaded data from",file_name) #temp
-    return ret
 
 
 def load_items():
@@ -25,6 +29,8 @@ def load_items():
         item = parse(val)
         if not item == None:
             items.append(item)
+        else:
+            print("Error loading item",val)
 
 def start():
     load_items()
@@ -32,6 +38,7 @@ def start():
 def main():
     #make the rendering in this file or another one if you want
     # also pls replace the print text with like on screen stuff
+    tk_run()
     print("Press enter to start!") # you can replace enter with a butto
     input("")
     start()
