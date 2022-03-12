d02d8ba75919bff776cb647672c32d6cc28f7829
ArjunDendukuri <arjundendukuri@kchigh.com>
2022-03-12 16:57:51 +0530

    Add Description and Fix Bugs

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-from item.JsonParser import load_items
+import json
+from item.Item import Item
+from item.Item import json_paths
+from tkinter import *
+
+items = []
+
+
+def parse(file_name):
+    try:
+        file = open(file_name,'r')
+    except:
+        print("File {} is not found".format(file_name))
+        return None
+    raw_data = file.read()
+    json_data = json.dumps(raw_data)
+    ret = Item(json_data['name'], json_data['description'], json_data['rarity']) 
+    file.close()
+    print("Loaded data from",file_name) #temp
+    return ret
+
+
+def load_items():
+    for val in json_paths.values():
+        item = parse(val)
+        if not item == None:
+            items.append(item)
 
 def start():
     load_items()
@@ -9,9 +35,9 @@ def main():
     print("Press enter to start!") # you can replace enter with a butto
     input("")
     start()
+    
 
 
 if __name__ == "__main__":
     main()
-
 
