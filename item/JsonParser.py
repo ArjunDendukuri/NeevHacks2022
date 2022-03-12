7167a2db9ef165ff6dd58aba5c9b9e41c4c56df0
ArjunDendukuri <arjundendukuri@kchigh.com>
2022-03-12 14:56:35 +0530

    Add File Not Existing Protection

--- item/JsonParser.py
+++ item/JsonParser.py
@@ -1,5 +1,5 @@
 import json
-import Item
+from item.Item import Item
 
 json_paths = {
     "coca_cola_can": "json/coca_cola_can.json",
@@ -8,16 +8,22 @@ json_paths = {
 items = []
 
 
-def parse(file_name):
-    file = open(file_name,'r')
+def parse(file_name:str)-> Item | None:
+    try:
+        file = open(file_name,'r')
+    except:
+        print("File {} is not found".format(file_name))
+        return None
     raw_data = file.read()
     json_data = json.loads(raw_data)
-    item = Item(json_data['name'], json_data['description'], json_data['value']) 
+    item = item.Item(json_data['name'], json_data['description'], json_data['value']) 
     file.close()
     print("Loaded data from",file_name) #temp
     return item
 
 
-def load():
+def load_items():
     for val in json_paths.values():
-        items.append(parse(json_paths[val]))
+        item = parse(val)
+        if not item == None:
+            items.append(item)
